1f39f9419eb75a38a81c1a3f7c98de8fe8a8e21a
VitaliySaburdo <vsaburdo@gmail.com>
2023-06-25 15:28:46 +0300

    fix

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import {Button} from './Button.styled';
-
-export const Button = ({ children, ...props }) => {
-    return (
-        <>
-            <Button {...props}>{children }</Button>
-        </>
-    )
-}
