0cdc25196fe78e7563fe554c6b3ebfd435a42549
VitaliySaburdo <vsaburdo@gmail.com>
2023-06-26 10:03:20 +0300

    fix container

--- src/components/ContactsMenu/ContactsMenu.styled.jsx
+++ src/components/ContactsMenu/ContactsMenu.styled.jsx
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 
 export const ContactContainer = styled.div`
-display: flex;
-
-`
+  display: flex;
+`;
 
 export const FormContainer = styled.div`
-margin-top: 60px;
-padding: 20px;
-border: 1px solid;
-border-radius: 8px;
-border-color: rgba(0,0,0,0.1);
-box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.07);
-`
+  margin-top: 60px;
+  height: 420px;
+  padding: 20px;
+  border: 1px solid;
+  border-radius: 8px;
+  border-color: rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.07);
+`;
