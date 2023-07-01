68083bb48b9ee54a40fe1d2f42044e487429da64
VitaliySaburdo <vsaburdo@gmail.com>
2023-07-01 11:52:27 +0300

    fix media

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,11 @@ import { Hero } from '../components/Hero/Hero';
 import { Section } from 'components/Section/Section';
 
 export default function Home() {
-
   return (
     <>
       <Section>
-      <Container>
-        <Hero />
+        <Container>
+          <Hero />
         </Container>
       </Section>
     </>
