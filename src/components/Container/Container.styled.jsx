6f40c9d5daca0324660c5ec61c5f7941be46e056
VitaliySaburdo <vsaburdo@gmail.com>
2023-07-02 20:47:23 +0300

    container

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledContainer = styled.div`
   margin: 0 auto;
   padding: 0 10px;
-width: 320px;
+  width: 320px;
 
   @media screen and (${props => props.theme.media.md}) {
     width: 768px;
